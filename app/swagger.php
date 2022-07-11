80f817e005b50f40fc8a0fb106daf50c60fdbbdb
kinoshita0923 <sota0923ks15@gmail.com>
2022-07-11 06:10:36 +0000

    API仕様書を自動化

---
+++ app/swagger.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @OA\Info(
+ *  title="API Example",
+ *  description="description",
+ *  version="1.0.0",
+ * )
+ * 
+ *  @OA\Server(
+ *   description="OpenApi host",
+ *   url="http://localhost:8080"
+ * )
+ */
