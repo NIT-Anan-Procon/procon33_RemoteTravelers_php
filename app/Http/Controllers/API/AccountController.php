ad119dabfc9d6050b36877505795e70062c8fd6e
kinoshita0923 <sota0923ks15@gmail.com>
2022-08-17 21:45:34 +0900

    ユーザ登録のAPIを実装

---
+++ app/Http/Controllers/API/AccountController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Account;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AccountController extends Controller
+{
+    public function signup() {
+        try {
+            $data = Account::insertGetId([
+                'user_id' => null
+            ]);
+
+            $result = [
+                'ok' => true,
+                'data' => $data,
+                'error' => null
+            ];
+            return $this->resConversionJson($result);
+        } catch (Exception $e) {
+            DB::rollBack();
+            $result = [
+                'ok' => false,
+                'data' => null,
+                'error' => $e->getMessage()
+            ];
+            return $this->resConversionJson($result, $e->getCode());
+        }
+    }
+
+    private function resConversionJson($result, $statusCode=200)
+    {
+        if(empty($statusCode) || $statusCode < 100 || $statusCode >= 600){
+            $statusCode = 500;
+        }
+        return response()->json($result, $statusCode, ['Content-Type' => 'application/json'], JSON_UNESCAPED_SLASHES);
+    }
+}
---
+++ database/migrations/2022_08_17_121618_create_accounts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->bigIncrements('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
